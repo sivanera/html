8239bc2e62f5454fad302117710d3e16667961c3
sivaneras <sivaneras@yandex.ru>
2024-02-13 17:27:54 +0500

    secnd tsk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import re
 
 app = Flask(__name__)
 
@@ -8,6 +9,9 @@ app = Flask(__name__)
 def index(title='Страница подготовки к миссии на Марс'):
     return render_template('base.html', title=title)
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
 
 if __name__ == '__main__':
     app.run()
