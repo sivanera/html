4e2f11c68a5bfb9584bec5e870e960f56f20218a
sivaneras <sivaneras@yandex.ru>
2024-02-13 17:34:08 +0500

    thrd tsk

--- main.py
+++ main.py
@@ -9,9 +9,20 @@ app = Flask(__name__)
 def index(title='Страница подготовки к миссии на Марс'):
     return render_template('base.html', title=title)
 
+
 @app.route('/training/<prof>')
 def training(prof):
     return render_template('training.html', prof=prof)
+
+
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    if list in ['ol', 'ul']:
+        professions = ['Инженер', 'Ученый', 'Пилот', 'Медик', 'Биолог']
+        return render_template('list_prof.html', list=list, professions=professions)
+    else:
+        return render_template('list_prof.html', list='error')
+
 
 if __name__ == '__main__':
     app.run()
